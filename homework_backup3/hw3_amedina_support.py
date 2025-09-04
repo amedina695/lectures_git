9969fa7f1711c7e1e19ba954011d5f9ec2135a29
amedina695 <al864695@ucf.edu>
2025-09-04 13:24:49 -0400

    hw3_first

---
+++ homework_backup3/hw3_amedina_support.py
@@ -0,0 +1,47 @@
+"""This file is titled support.py, with class naming convention, 
+with the purpose of being used to add support functions for homework
+assignments.
+
+"""
+
+# Imports
+
+import os # standard library imports first
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import astropy.io.fits as fits
+
+# My functions
+# Creating a square function
+
+def square(x):
+    """A function that takes an input of a scalar
+    or an array if any dimension or type and returns
+    the square.
+
+    Parameters
+    -----------
+    x: int or array_like
+
+    Returns
+    -------
+    int or array
+
+    References
+    ----------
+    https://numpy.org/doc/2.2/reference/generated/numpy.asarray.html
+
+    Examples
+    --------
+    >>> a=[1,2,3]
+    >>> print(square(a))
+    [1, 4, 9]
+
+    """
+
+    x = np.asarray(x) # Converts input to array
+    return x**2
+
+    pass
