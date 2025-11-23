c535f63e5a6f158279c65b91c0eca7d5ca524665
amedina695 <al864695@ucf.edu>
2025-11-23 16:09:21 -0500

    final_daily

---
+++ final/final_amedinasupport.py
@@ -0,0 +1,14 @@
+# final_amedinasupport.py
+
+# Imports
+import os
+import disk as d
+import numpy as np
+import gaussian as g
+import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+from astropy.stats import sigma_clip
+
+
+
+
